fd404a5d21f20755b3948a46de311644047cb886
ndalessio <noelidalessio@gmail.com>
2020-08-08 20:41:50 -0300

    paint working

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const colors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+const colors = ["Lightcoral", "Salmon", "Lightsalmon", "Pink", "Lightpink", "Hotpink", "Deeppink", "Violet","Mediumvioletred", "Red", "Firebrick", "Darkred", "Orange", "Darkorange", "Yellow", "Lightyellow", "Lemonchiffon", "Greenyellow", "Lawngreen", "Lime", "Limegren", ""]
 
 const pallete = document.getElementById("pallete");
+const selected = document.getElementById("selected");
 const grill = document.getElementById("grill");
+const deleteButton = document.getElementById("delete-everything");
+const paintBackground = document.getElementById("paint-background");
 const pixels = [];
 
 // Creating the grill
@@ -28,15 +31,61 @@ const createPallete = () => {
         colorPallete.classList.add("color");
         colorPallete.style.backgroundColor = color;
 
+        colorPallete.onclick = () => {
+            selectColor(color)
+        }
+
         pallete.appendChild(colorPallete);
     }
 }
 
-const selectColor = () => {
-    
+// Color selected
+const selectColor = (color) => {
+    actualColor = color;
+    selected.style.backgroundColor = actualColor;
 }
 
+// Grill
 
-createGrill();
+grill.onclick = () => {
+    if (event.target.classList.contains("pixel")){
+        event.target.style.backgroundColor = actualColor;
+    }
+}
+
+grill.onmousemove = () => {
+    if (!mouseDown)
+        return;
+
+    if (event.target.classList.contains("pixel")) {
+        event.target.style.backgroundColor = actualColor;
+    }
+}
+
+document.onmousedown = () => {
+    mouseDown = true;
+}
+  
+  document.onmouseup = () => {
+    mouseDown = false;
+}
+
+// Buttons
+
+deleteButton.onclick = () => {
+    for(let pixel of pixels){
+        pixel.style.backgroundColor = "White";
+    }
+}
+
+paintBackground.onclick = () => {
+    for(let pixel of pixels){
+        if (pixel.style.backgroundColor === "white"){
+            pixel.style.backgroundColor = actualColor;
+        }
+    }
+}
+
+createGrill(100,100);
 createPallete();
 
