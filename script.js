94ddc9e9c37fb0f113fe255e138b2b7bbd737e5a
ndalessio <noelidalessio@gmail.com>
2020-08-08 16:14:19 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const colors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+
+const pallete = document.getElementById("pallete");
+const grill = document.getElementById("grill");
+const pixels = [];
+
+// Creating the grill
+const createGrill = (rows, columns) => {
+    for(let i = 0; i < rows; i++){
+        const row = document.createElement("row")
+        row.classList.add("row")
+
+        for(let i = 0; i < columns; i++){
+            const pixel = document.createElement("div")
+            pixel.classList.add("pixel");
+            pixel.style.backgroundColor = 'White';
+            row.appendChild(pixel);
+            pixels.push(pixel)
+        }
+        grill.appendChild(row)
+    }
+}
+
+// Creating the color Pallete
+const createPallete = () => {
+    for (let color of colors){
+        const colorPallete = document.createElement("div");
+        colorPallete.classList.add("color");
+        colorPallete.style.backgroundColor = color;
+
+        pallete.appendChild(colorPallete);
+    }
+}
+
+const selectColor = () => {
+    
+}
+
+
+createGrill();
+createPallete();
+
